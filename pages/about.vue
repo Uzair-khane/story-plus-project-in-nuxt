3948729f010e4ee53448631c580d4225d529a6b9
Uzair-Khane <uzairkhanar123@gmail.com>
2025-06-30 01:06:31 +0100

    add about page

--- pages/about.vue
+++ pages/about.vue
@@ -1,6 +1,63 @@
 <script>
+import Header from "~/components/Header.vue";
 </script>
 
 <template>
-<div class="text-8xl text-red-700">about page</div>
+  <div>
+    <Header />
+    <div class="pt-[80px] p-12">
+      <h1 class="text-2xl font-semibold text-slate-800">About us</h1>
+      <h2 class="text-[20px] pt-4 font-semibold text-slate-800">
+        Welcome to StoryPulse, where stories come alive!
+      </h2>
+      <p class="text-[16px] pt-4 text-slate-800">
+        At StoryPulse, we believe in the power of storytelling to inspire,
+        entertain, and connect people from all walks of life. Our mission is to
+        bring you captivating tales, thought-provoking narratives, and
+        heartwarming adventures that resonate with readers around the globe
+      </p>
+      <h2 class="text-[20px] pt-4 font-semibold text-slate-800">Who We Are</h2>
+      <p class="text-[16px] pt-4 text-slate-800">
+        We are a passionate team of writers, editors, and storytellers dedicated
+        to sharing the beauty and diversity of human experiences. From fiction
+        to non-fiction, short stories to serialized novels, our platform is a
+        haven for literary enthusiasts seeking fresh and engaging content.
+      </p>
+      <h2 class="text-[20px] pt-4 font-semibold text-slate-800">What We Do</h2>
+      <ul class="list-disc pl-6">
+        <li>
+          <span class="text-[17px] mt-5 font-semibold text-slate-800"
+            >Curate Quality Content:
+          </span>
+          We handpick stories from talented writers, ensuring a diverse range of
+          voices and perspectives.
+        </li>
+        <li>
+          <span class="text-[17px] mt-5 font-semibold text-slate-800"
+            >Encourage Creativity:</span
+          >
+          We provide a platform for aspiring authors to showcase their work and
+          reach a wider audience.
+        </li>
+        <li>
+          <span class="text-[17px] mt-5 font-semibold text-slate-800"
+            >Foster a Community:</span
+          >
+          We believe in building a supportive community where readers and
+          writers can connect, share feedback, and grow together.
+        </li>
+      </ul>
+      <h2 class="text-[20px] pt-4 font-semibold text-slate-800">
+        Join Us on This Journey
+      </h2>
+      <p class="text-[16px] pt-4 text-slate-800">
+        Whether you’re a voracious reader looking for your next great read or a
+        writer with a story to tell, StoryPulse is the perfect place for you.
+        Dive into our collection, submit your own stories, and be part of a
+        vibrant community that celebrates the art of storytelling. Thank you for
+        visiting StoryPulse. We hope you find inspiration, joy, and a sense of
+        belonging in our stories.
+      </p>
+    </div>
+  </div>
 </template>
